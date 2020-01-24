b9878b41149f8d09445c615fe8d09762ea09a828
Liam Gower <lee.gower17@gmail.com>
2020-01-24 21:39:56 +0000

    gameweek margins

--- fpl_draft_league.py
+++ fpl_draft_league.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pandas.io.json import json_normalize
 import matplotlib.pyplot as plt
 import datetime
+import numpy as np
 
 
 def get_json(json_file, api, email_address):
@@ -154,7 +155,110 @@ def get_points_over_time(matches_df, league_entry_df):
     return output_df, matches_df_stacked, pivot_df
 
 
+def get_matches_stacked(matches_df, league_entry_df):
+    
+    # Limit to finished games only
+    matches_df = matches_df[matches_df['finished'] == True]
 
+    # Join to get team names and player names of entry 1 (home team)
+    matches_df = pd.merge(matches_df,
+                          league_entry_df[['id', 'player_first_name']],
+                          how='left',
+                          left_on='league_entry_1',
+                          right_on='id')
+
+    # Join to get team names and player names of entry 2 (away team)
+    matches_df = pd.merge(matches_df,
+                          league_entry_df[['id', 'player_first_name']],
+                          how='left',
+                          left_on='league_entry_2',
+                          right_on='id')
+
+    # Drop unused columns, rename for clearer columns
+    matches_df = (matches_df
+                 .drop(['finished', 'started', 'id_x', 'id_y', 'league_entry_1', 'league_entry_2'], axis=1)
+                .rename(columns={'event':'match',
+                           'player_first_name_x': 'home_player',
+                           'league_entry_1_points': 'home_score',
+                           'player_first_name_y': 'away_player',
+                           'league_entry_2_points': 'away_score',
+                          })
+                )
+    
+    def calc_points(df):
+        if df['home_score'] == df['away_score']:
+            df['home_points'] = 1
+            df['away_points'] = 1
+        elif df['home_score'] > df['away_score']:
+            df['home_points'] = 3
+            df['away_points'] = 0
+        else:
+            df['home_points'] = 0
+            df['away_points'] = 3
+            
+        df['home_margin'] = df['home_score'] - df['away_score']
+        df['away_margin'] = df['away_score'] - df['home_score']
+        
+        return df
+
+    matches_df = matches_df.apply(calc_points, axis=1)
+    
+    home_df = matches_df[['match', 'home_player', 'home_score', 'home_points', 'home_margin']]
+    home_df = home_df.rename(columns={'home_player':'team', 'home_score':'score', 'home_points':'points', 'home_margin':'margin'})
+
+    away_df = matches_df[['match', 'away_player', 'away_score', 'away_points', 'away_margin']]
+    away_df = away_df.rename(columns={'away_player':'team', 'away_score':'score', 'away_points':'points', 'away_margin':'margin'})
+
+    matches_df_stacked = home_df.append(away_df)
+    matches_df_stacked = matches_df_stacked.sort_values(by='match').reset_index().drop(['index'], axis=1)
+    
+    return matches_df_stacked
+
+
+def chart_margins_single(df, player):
+    
+    df = df[df['team'] == player]
+    my_colour = np.where(df['margin']>0, '#00ff85', '#e90052')
+    plt.figure(figsize=(10,6))
+
+    plt.bar(df['match'], df['margin'], color=my_colour)
+
+    ax = plt.gca()
+    ax.set_xticks(range(1, len(df) + 1, 1))
+    ax.set_yticks(range(-50,50,10))
+    ax.set_xticklabels(range(1, len(df) + 1, 1))
+    ax.set_xlabel('Gameweek #')
+    ax.set_ylabel('Points margin')
+    ax.set_title('Gameweek Points Margins')
+    ax.grid(True, color='778899', alpha=0.1, axis='y')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+def chart_margins_multi(df, name_dict):
+    
+    fig1, ax = plt.subplots(3,3, figsize=(50,20), sharex='col', sharey='row')
+
+    fig1.suptitle("Gameweek Margins by player", fontsize=30)
+
+    counter = 0
+    for i in range(3):
+        for j in range(3):
+            my_colour = np.where(df[df['team'] == name_dict[counter]]['margin']>0, '#00ff85', '#e90052')
+            ax[i, j].bar(df[df['team'] == name_dict[counter]]['match'],
+                         df[df['team'] == name_dict[counter]]['margin'],
+                         color=my_colour
+                        )
+            ax[i,j].set_title(f"{name_dict[counter]}", fontsize=20)
+            ax[i,j].set_xticks(range(1, len(df[df['team'] == name_dict[counter]]) + 1, 1))
+            ax[i,j].set_yticks(range(-60,60,10))
+            ax[i,j].set_xticklabels(range(1, len(df[df['team'] == name_dict[counter]]) + 1, 1),fontsize=10)
+            ax[i,j].set_xlabel('Gameweek #',fontsize=15)
+            ax[i,j].set_ylabel('Points margin',fontsize=15)
+            ax[i,j].grid(True, color='778899', alpha=0.1, axis='y')
+            ax[i,j].spines['right'].set_visible(False)
+            ax[i,j].spines['top'].set_visible(False)
+
+            counter += 1
 
 
 def get_streaks(matches_df_stacked):
